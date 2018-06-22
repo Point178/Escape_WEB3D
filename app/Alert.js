b768a8b88727b1eff610614cf30e74f83659211c
lyx <15302010043@fudan.edu.cn>
2018-06-22 18:01:18 +0800

    令用户名不超过8位
    未进行webpack

--- app/Alert.js
+++ app/Alert.js
@@ -1,13 +1,7 @@
 function InputCheck(register) {
-    if (register.username.value == "") {
-        alert("用户名不能为空！");
+    if(register.username.value.length > 8){
+        alert("用户名不能超过8位！");
         register.username.focus();
-        return false;
-    }
-
-    if (register.password.value.length <= 6 || typeof (register.password.value) == "number") {
-        alert("用户设置的密码必须比6位大并且不能是纯数字！");
-        register.password.focus();
         return false;
     }
 }
--- app/register.js
+++ app/register.js
@@ -1,2 +1,4 @@
 require('./bootstrap.min.css');
 require('./style.css');
+const Alert = require('./Alert.js');
+Alert();
